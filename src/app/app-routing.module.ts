eba482561256cb82e22ea37a5f240096a8544815
TiagoECO <tiago1023@hotmail.com>
2022-12-21 09:59:30 -0300

    up commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
+import { DetalhamentoComponent } from './componentes/detalhamento/detalhamento.component';
+import { FrenteComponent } from './componentes/frente/frente.component';
+import { UnidadeComponent } from './componentes/unidade/unidade.component';
+import { CiclosComponent } from './componentes/ciclos/ciclos.component';
+import { HomeComponent } from './componentes/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "ciclos",
+    component: CiclosComponent
+  },
+  {
+    path: "unidade",
+    component: UnidadeComponent
+  },
+  {
+    path: "frente",
+    component: FrenteComponent
+  },
+  {
+    path: "detalhamento",
+    component: DetalhamentoComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { CiclosComponent } from './componentes/ciclos/ciclos.component';
+import { UnidadeComponent } from './componentes/unidade/unidade.component';
+import { FrenteComponent } from './componentes/frente/frente.component';
+import { DetalhamentoComponent } from './componentes/detalhamento/detalhamento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CiclosComponent,
+    UnidadeComponent,
+    FrenteComponent,
+    DetalhamentoComponent
   ],
   imports: [
     BrowserModule,
